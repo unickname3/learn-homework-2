12b07372e285e90750721dceda65656282509dad
Igor Dikarev <igor.dikarev@gmail.com>
2023-05-21 00:25:50 +0300

    solve files

--- 2_files.py
+++ 2_files.py
@@ -11,12 +11,23 @@
 5. Сохраните результат в файл referat2.txt
 """
 
+
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    with open("referat.txt", "r", encoding="utf-8") as input_file:
+        data = input_file.read()
+
+    print(f"Длина получившейся строки {len(data)}")
+    print(f"Количество слов в тексте {len(data.split())}")
+
+    data2 = data.replace(".", "!")
+
+    with open("referat2.txt", "w", encoding="utf-8") as output_file:
+        output_file.write(data2)
+
 
 if __name__ == "__main__":
     main()
